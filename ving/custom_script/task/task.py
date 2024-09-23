a15a5c0be566a559fcfae0af8e6dcff815e0edc7
ahmad406 <73885085+ahmad406@users.noreply.github.com>
2024-09-23 22:20:14 +0530

    Update task.py

--- ving/custom_script/task/task.py
+++ ving/custom_script/task/task.py
@@ -4,17 +4,12 @@ from erpnext.projects.doctype.task.task import *
 import frappe
 from frappe import _
 class CustomTask(Task):
-	def validate(self):
-		frappe.throw("test")
 	@frappe.whitelist()
 	def get_assigment_status(self):
 			status=False
-			if self.is_template and self.status != "Template":
-				self.status = "Template"
-			if self.status != self.get_db_value("status") and self.status == "Completed":
-				for d in self.depends_on:
-					if frappe.db.get_value("Task", d.task, "status") not in ("Completed", "Cancelled"):
-						status=True
+			for d in self.depends_on:
+				if frappe.db.get_value("Task", d.task, "status") not in ("Completed", "Cancelled"):
+					status=True
 			return status
 						
 
