8848b11b14a84c1e49c2c357f511b05e1e792adf
ahmad406 <73885085+ahmad406@users.noreply.github.com>
2024-09-23 21:59:00 +0530

    Update hooks.py

--- ving/hooks.py
+++ ving/hooks.py
@@ -114,9 +114,9 @@ app_license = "mit"
 # ---------------
 # Override standard doctype classes
 
-# override_doctype_class = {
-#	"ToDo": "custom_app.overrides.CustomToDo"
-# }
+override_doctype_class = {
+"Task":"ving.custom_script.task.task.CustomTask"
+}
 
 # Document Events
 # ---------------
