b794ce3bcae64021cd7b0569a4459ffe53ee1f7f
ahmad406 <73885085+ahmad406@users.noreply.github.com>
2024-09-30 20:12:44 +0530

    Update to_do.py

--- ving/custom_script/to_do/to_do.py
+++ ving/custom_script/to_do/to_do.py
@@ -1,15 +1,22 @@
 import frappe
 @frappe.whitelist()
 def validate(self,method):
-  pass
+	if self.reference=="Task":
+		sql="""select * from `tabTask Depends On` where parent="{0}" """.format(self.reference_name)
+		for d in frappe.db.sql(sql, as_dict=True):
+			create_todo(d.subject,"Task",d.task,self)
 
 
 
-def create_todo(description, reference_type, reference_name):
+def create_todo(description, reference_type, reference_name,self):
 	todo = frappe.new_doc("ToDo")
 	todo.description = description
-	todo.owner = "Administrator"
+	todo.date = self.date
+	todo.priority =self.priority
+	todo.assigned_by = self.assigned_by
+	todo.owner = self.owner
+	todo.assigned_by_full_name = self.assigned_by_full_name
+
 	todo.reference_type = reference_type
 	todo.reference_name = reference_name
 	todo.insert()
-
