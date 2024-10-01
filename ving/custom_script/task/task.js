b586d4f7df80337e7cc96449c219f56a9e7ecea7
ahmad406 <73885085+ahmad406@users.noreply.github.com>
2024-10-01 10:50:25 +0530

    Update task.js

--- ving/custom_script/task/task.js
+++ ving/custom_script/task/task.js
@@ -1,17 +1,19 @@
 frappe.ui.form.on('Task', {
 	refresh: function (frm) {
-		frappe.call({
-			method: "get_assigment_status",
-			doc: cur_frm.doc,
-			callback: function (r) {
-				if (r) {
-					console.log(r.message)
-					if (r.message){
-						$(".add-assignment-btn").css("display", "none");
+		if (cur_frm.doc.parent_task) {
+			frappe.call({
+				method: "get_assigment_status",
+				doc: cur_frm.doc,
+				callback: function (r) {
+					if (r) {
+						console.log(r.message)
+						if (r.message) {
+							$(".add-assignment-btn").css("display", "none");
+						}
 					}
 				}
-			}
-		});
+			});
+		}
 	}
 
 })
