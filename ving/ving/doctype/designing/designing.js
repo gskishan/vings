c7904f8239eaf5b2dcb46db8107b9c84eb55e526
gskishan <gskishan123@gmail.com>
2024-10-28 22:54:06 +0530

    desining created

--- ving/ving/doctype/designing/designing.js
+++ ving/ving/doctype/designing/designing.js
@@ -18,6 +18,58 @@ frappe.ui.form.on("Designing equipment", {
         } 
     
         frm.refresh_field('equipment');
+    },
+    tr:function(frm,cdt,cdn){
+        const row = locals[cdt][cdn];
+        console.log(row.tr*row.qtyy)
+        if (row.tr && row.qty){
+            row.total_tr=row.tr*row.qty
+        }
+        cur_frm.refresh_fields()
+    },
+    qty:function(frm,cdt,cdn){
+        const row = locals[cdt][cdn];
+        console.log(row.tr*row.qtyy)
+        if (row.tr && row.qty){
+            row.total_tr=row.tr*row.qty
+        }
+        cur_frm.refresh_fields()
+    },
+    length:function(frm,cdt,cdn){
+        const row = locals[cdt][cdn];
+        if (row.length && row.width){
+            row.area=row.length*row.width
+        }
+        cur_frm.refresh_fields()
+    },
+    width:function(frm,cdt,cdn){
+        const row = locals[cdt][cdn];
+        if (row.length && row.width){
+            row.area=row.length*row.width
+        }
+        cur_frm.refresh_fields()
+    }
+
+
+
+});
+
+frappe.ui.form.on("Designing Low Side", {
+    quantity: function(frm, cdt, cdn) {
+        const row = locals[cdt][cdn]; 
+        if (row.rate && row.quantity){
+            row.amount=row.quantity*row.rate
+
+        }
+        cur_frm.refresh_fields()
+    },
+    rate: function(frm, cdt, cdn) {
+        const row = locals[cdt][cdn]; 
+        if (row.rate && row.quantity){
+            row.amount=row.quantity*row.rate
+
+        }
+        cur_frm.refresh_fields()
     }
 
 
