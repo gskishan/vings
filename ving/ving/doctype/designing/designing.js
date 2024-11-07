3f300df4649c5712cacb26b197e85e6f9fa6c370
gskishan <gskishan123@gmail.com>
2024-11-07 12:22:55 +0530

    desinging

--- ving/ving/doctype/designing/designing.js
+++ ving/ving/doctype/designing/designing.js
@@ -2,50 +2,50 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Designing", {
-	refresh(frm) {
+    refresh(frm) {
 
-	},
-    
+    },
+
 });
 frappe.ui.form.on("Designing equipment", {
-    equipment_add: function(frm, cdt, cdn) {
-        const row = locals[cdt][cdn]; 
-        const previous_rows = frm.doc.equipment; 
-    
+    equipment_add: function (frm, cdt, cdn) {
+        const row = locals[cdt][cdn];
+        const previous_rows = frm.doc.equipment;
+
         if (previous_rows.length > 1) {
-            const last_row = previous_rows[previous_rows.length - 2]; 
-            row.floor = last_row.floor; 
-        } 
-    
+            const last_row = previous_rows[previous_rows.length - 2];
+            row.floor = last_row.floor;
+        }
+
         frm.refresh_field('equipment');
     },
-    tr:function(frm,cdt,cdn){
+    tr: function (frm, cdt, cdn) {
         const row = locals[cdt][cdn];
-        console.log(row.tr*row.qtyy)
-        if (row.tr && row.qty){
-            row.total_tr=row.tr*row.qty
+        console.log(row.tr * row.qtyy)
+        if (row.tr && row.qty) {
+            row.total_tr = row.tr * row.qty
         }
         cur_frm.refresh_fields()
     },
-    qty:function(frm,cdt,cdn){
+    qty: function (frm, cdt, cdn) {
         const row = locals[cdt][cdn];
-        console.log(row.tr*row.qtyy)
-        if (row.tr && row.qty){
-            row.total_tr=row.tr*row.qty
+        console.log(row.tr * row.qtyy)
+        if (row.tr && row.qty) {
+            row.total_tr = row.tr * row.qty
         }
         cur_frm.refresh_fields()
     },
-    length:function(frm,cdt,cdn){
+    length: function (frm, cdt, cdn) {
         const row = locals[cdt][cdn];
-        if (row.length && row.width){
-            row.area=row.length*row.width
+        if (row.length && row.width) {
+            row.area = row.length * row.width
         }
         cur_frm.refresh_fields()
     },
-    width:function(frm,cdt,cdn){
+    width: function (frm, cdt, cdn) {
         const row = locals[cdt][cdn];
-        if (row.length && row.width){
-            row.area=row.length*row.width
+        if (row.length && row.width) {
+            row.area = row.length * row.width
         }
         cur_frm.refresh_fields()
     }
@@ -55,18 +55,18 @@ frappe.ui.form.on("Designing equipment", {
 });
 
 frappe.ui.form.on("Designing Low Side", {
-    quantity: function(frm, cdt, cdn) {
-        const row = locals[cdt][cdn]; 
-        if (row.rate && row.quantity){
-            row.amount=row.quantity*row.rate
+    quantity: function (frm, cdt, cdn) {
+        const row = locals[cdt][cdn];
+        if (row.rate && row.quantity) {
+            row.amount = row.quantity * row.rate
 
         }
         cur_frm.refresh_fields()
     },
-    rate: function(frm, cdt, cdn) {
-        const row = locals[cdt][cdn]; 
-        if (row.rate && row.quantity){
-            row.amount=row.quantity*row.rate
+    rate: function (frm, cdt, cdn) {
+        const row = locals[cdt][cdn];
+        if (row.rate && row.quantity) {
+            row.amount = row.quantity * row.rate
 
         }
         cur_frm.refresh_fields()
--- ving/ving/doctype/designing/designing.py
+++ ving/ving/doctype/designing/designing.py
@@ -2,11 +2,48 @@
 # For license information, please see license.txt
 import frappe
 from frappe.model.document import Document
+from collections import defaultdict
 
 
 class Designing(Document):
 	def validate(self):
 		self.fill_bill()
+		self.get_totals()
+
+
+
+
+	def get_totals(self):
+		floor_totals = defaultdict(lambda: {"capacity": 0, "qty": 0, "total_tr": 0, "tr": 0})
+
+		for item in self.equipment:
+			floor = item.get("floor")
+			floor_totals[floor]["capacity"] += item.get("capacity", 0)
+			floor_totals[floor]["qty"] += item.get("qty", 0)
+			floor_totals[floor]["tr"] += item.get("tr", 0)
+			floor_totals[floor]["total_tr"] += item.get("total_tr", 0)
+
+
+		self.designing_total=[]
+
+		for floor, totals in floor_totals.items():
+			
+			row=self.append("designing_total",{})
+			row.floor=floor
+			row.total_capacity_index=totals['capacity']
+			row.total_tr=totals['total_tr']
+			# row.total_hp=totals['total_hp']  #need to add
+			row.total_qty=totals['qty']  
+			row.hp=totals['hp']
+			# row.odu_capacity=totalsd
+
+
+			# print(f"Floor: {floor}")
+			# print(f"  Total Capacity: {totals['capacity']}")
+			# print(f"  Total Quantity: {totals['qty']}")
+			# print(f"  Total TR: {totals['tr']}")
+			# print(f"  Total Total_TR: {totals['total_tr']}")
+
 
 	def fill_bill(self):
 		data=self.sum_item()
