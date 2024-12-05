4034a2613b4e3c3b6dab37e3d868651bf720d1be
ahmad406 <73885085+ahmad406@users.noreply.github.com>
2024-12-05 10:40:28 +0530

    Update item_price_list.py

--- ving/custom_script/item_price_list/item_price_list.py
+++ ving/custom_script/item_price_list/item_price_list.py
@@ -40,7 +40,7 @@ def validate(self,method=None):
 
 
 def  checkif(self):
-    sql="""select name from `tabItem price summmary` where item_price="{0}" and parent="{1}" """.format(self.name,self.item_code)
+    sql="""select name from `tabItem price summmary` where price_list="{0}" and parent="{1}" """.format(self.name,self.item_code)
     data =frappe.db.sql(sql,as_dict=1)
     if data:
         return data[0].name
