722f65d77c6a6fc913fb6295e6e0a5e68dc1b15f
ahmad406 <73885085+ahmad406@users.noreply.github.com>
2025-01-06 22:08:14 +0530

    Update task.py

--- ving/custom_script/task/task.py
+++ ving/custom_script/task/task.py
@@ -11,5 +11,49 @@ class CustomTask(Task):
 				if frappe.db.get_value("Task", d.task, "status") not in ("Completed", "Cancelled"):
 					status=True
 			return status
-						
+
+	def validate(self):
+		self.validate_dates()
+		self.validate_progress()
+		self.validate_status()
+		self.update_depends_on()
+		self.validate_dependencies_for_template_task()
+		self.validate_completed_on()
+		self.update_parent_status()
+
+
+
+	def update_parent_status(self):
+		current_status = frappe.db.get_value("Task", self.name, "status")
+		if (
+			self.parent_task
+			and self.status == "Completed"
+			and current_status != "Completed"
+		):
+			sql = """
+				SELECT COUNT(name) AS incomplete_count
+				FROM `tabTask`
+				WHERE parent_task = %s
+				AND name != %s
+				AND status != 'Completed'
+			"""
+			incomplete_tasks = frappe.db.sql(sql, values=(self.parent_task, self.name), as_dict=True)
+
+			if incomplete_tasks[0].incomplete_count == 0:
+				frappe.db.set_value("Task", self.parent_task, "status", "Completed")
+
+		elif (
+			self.parent_task
+			and current_status == "Completed"
+			and self.status != "Completed"
+		):
+			frappe.db.set_value("Task", self.parent_task, "status", self.status)
+
+
+
+					
+
+			
+
+							
 
