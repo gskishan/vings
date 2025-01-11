17d0fbc75b0fd1233ea4a38e08e5e87db1b0d6dd
ahmad406 <73885085+ahmad406@users.noreply.github.com>
2025-01-11 12:22:40 +0530

    Update stock_entry.js

--- ving/custom_script/stock_entry/stock_entry.js
+++ ving/custom_script/stock_entry/stock_entry.js
@@ -1,6 +1,6 @@
 frappe.ui.form.on("Stock Entry", {
     refresh: function (frm, doc, dt, dn) {
-        frm.add_custom_button(__('Stock Entry'),
+        frm.add_custom_button(__('Sales Order'),
             function () {
                 if (!cur_frm.doc.project) {
                     frappe.throw({
@@ -14,7 +14,7 @@ frappe.ui.form.on("Stock Entry", {
                         source_doctype: "Sales Order",
                         target: frm,
                         setters: {
-                            custom_project: frm.doc.project,
+                            project: frm.doc.project,
                         },
                         get_query_filters: {
                             project: frm.doc.project,
