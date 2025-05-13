297f11f2746a65510d23c56e4fe45c4fcc84619e
G S Kishan <95840962+gskishan@users.noreply.github.com>
2025-05-13 21:01:14 +0530

    Update salary_slip.py

--- ving/custom_script/salary_slip/salary_slip.py
+++ ving/custom_script/salary_slip/salary_slip.py
@@ -351,7 +351,7 @@ class CustomSalarySlip(SalarySlip):
 
 
 						no_of_holiday=flt(len(holidays))
-						frappe.errprint([self.total_working_days,no_of_holiday,total_leaves_taken,self.leave_without_pay,self.absent_days,self.custom_worked_on_holiday])
+						# frappe.errprint([self.total_working_days,no_of_holiday,total_leaves_taken,self.leave_without_pay,self.absent_days,self.custom_worked_on_holiday])
 						component_row.amount=d.variable*(self.total_working_days-no_of_holiday-total_leaves_taken-self.leave_without_pay-self.absent_days+self.custom_worked_on_holiday)
 					if d.type=="Night Allowance":
 						component_row.amount=d.variable*350
